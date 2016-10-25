95aae7917c41a76ab67cfeb6f048a60a7540f5aa
Mooncode29 <irene.ravelonarivo@gmail.com>
2016-10-25 16:27:59 +0200

    ajout menu liens

--- js/app.js
+++ js/app.js
@@ -1,34 +1,59 @@
 (function(){
 	"use strict";
 	var app = {
+		ancre : null,
+		len: null,
+		objJson : null,
+
 		init:function(){
-			// let's go
+		
 			app.listners();
+			app.getFichierJson();
+
+			// app.getFichierJson();
+			// app.ajaxDone2();
+
+		},
+
+		getFichierJson : function(){
+			var lienJson = ("http://192.168.1.40:1337/menu.json");
+			$.ajax(lienJson)
+			.done(app.getTitle)
+			.fail(console.log('fail'));
+		},
+		
+		getTitle : function(json){
+			app.objJson = json;
+			console.log(app.objJson);
+			app.len = json.menu.length;
+			for(var i = 0; i < app.len; i++){
+				var titre = json.menu[i].title;	
+				var ancre = ' <a class="item">'+ titre + '</a>';
+				$("#titre_articles").append(ancre);
+			}
 		},
 
 		listners : function(){
-			$("#recuperer").on('click', app.getFichier );
+			$("a").on('click', function(){
 
+				app.getPath();
+			})
 		},
 
-		getFichier : function(){
-			var addressIp = ("http://192.168.1.21:1337/alice.md"); 
-			$.ajax(addressIp)
-			.done(app.ajaxDone)
-			.fail(app.ajaxFail);
-		},
-		ajaxDone : function(mdFile){
-			console.log(mdFile);
-			var converter = new showdown.Converter();
-			var htmlFile = converter.makeHtml(mdFile);
-			$("#md").append(htmlFile);
+		getPath : function(){
+			app.len = app.objJson.menu.lenght;
+			for(var i = 0; i < app.len; i++){
+				var article = json.menu[i].path;
+				console.log(article);
 
-		}
+			}
+		},
 
 	};
-
-
 	$(document).ready(function(){
 		app.init();
 	});
 })();
+			// var converter = new showdown.Converter();
+			// var htmlFile = converter.makeHtml(mdFile);
+			// $("#md").append(htmlFile)		
