485888f7d6152035e5f96352d23f2caba4cdbe42
Mooncode29 <irene.ravelonarivo@gmail.com>
2016-10-25 10:43:23 +0200

    etape 1 ok

--- js/app.js
+++ js/app.js
@@ -3,7 +3,28 @@
 	var app = {
 		init:function(){
 			// let's go
+			app.listners();
 		},
+
+		listners : function(){
+			$("#recuperer").on('click', app.getFichier );
+
+		},
+
+		getFichier : function(){
+			var addressIp = ("http://192.168.1.21:1337/alice.md"); 
+			$.ajax(addressIp)
+			.done(app.ajaxDone)
+			.fail(app.ajaxFail);
+		},
+		ajaxDone : function(mdFile){
+			console.log(mdFile);
+			var converter = new showdown.Converter();
+			var htmlFile = converter.makeHtml(mdFile);
+			$("#md").append(htmlFile);
+
+		}
+
 	};
 
 
