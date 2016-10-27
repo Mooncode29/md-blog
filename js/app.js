67b830677fa58a85c4d826758d522faf08cc9799
Mooncode29 <irene.ravelonarivo@gmail.com>
2016-10-27 12:29:15 +0200

    etapee 1 et 2 ok, avec quelques commentaires laissés

--- js/app.js
+++ js/app.js
@@ -3,59 +3,43 @@
 	console.log("start");
 	var app = {
 		url :'http://192.168.1.40:1337/',
-		init:function(){
-			// let's go
-			/*
-				Récupérer le fichier example.md du serveur distant
-					Serveur distant : */
-			var url = "http://192.168.1.40:1337/";
-			var urlExample = "example.md";
-			$.ajax(url + urlExample)
-			.done(app.ajaxDone)
-			.fail(app.ajaxFail);
-
-
-			/*
-				Transformez le md en html
-				Insérez le html dans la page dans  #md
-			*/
-			app.initMenu();
-		},
-		ajaxDone : function(data){
-			console.log("ajaxDone");
-			var md = data;
-			console.log(md);
-			var converter = new showdown.Converter();
-			var htmlexample = converter.makeHtml(md);
-			console.log(htmlexample);
-			$("#md").html(htmlexample);
-			
-
-		},
-		ajaxFail : function(){
-			console.log("ajaxFail");
-		},
-
 		initMenu : function(){
 			var urlJson = "menu.json";
 			$.ajax(this.url + urlJson)
 			.done(app.ajaxMenuDone)
 			.fail(app.ajaxMenuFail);
-			//app.listner();
 		},
+			
 		ajaxMenuDone: function(dataJson){
-			console.log('ajaxMenuDone');
-			console.log(dataJson);
-			// for (var i = 0; i < dataJson.menu.length; i++){
-			// 	$("#menu").append('<li>'<a href="#" data-path="+dataJson.menu[i].path+'">'+ dataJson.menu[i].title + '</a>'</li>');
-			// 	}
-// on clique sur le lien
-// récupérer le fichier contenant ce titre 
-// et on afficher le fichier
+			for (var i = 0; i < dataJson.menu.length; i++){
+				var article = dataJson.menu[i];
+				console.log(article);
+				var lienHtml = '<li><a href="#" class="liens" data-path="' + article.path +'">';
+				lienHtml += article.title;	
+				lienHtml += '</a></li>';
+				$('#liensArticles').append(lienHtml);
+			};
+			$('.liens').click(app.ouvreArticle);
 		},
 
-		listner : function(){
-			$("a").on('click',console.log('click') );
+		ouvreArticle : function(){
+			// console.log('ouvreArticle');
+			// console.log(this);
+			var lienJquery = $(this); //convertir notre noeud DOM en élément Jquery
+			console.log(lienJquery);
+			var path = lienJquery.data(path);
+			// console.log(path);
+			//faire requête ajax pour récupérer le fichier md
+			$.ajax( app.url + path.path)
+			.done(function(response){
+				console.log(response);
+			//convertir le response(fichier md) en html avec showdown;
+				var converter = new showdown.Converter();
+				var fichierHtml = converter.makeHtml(response);
+				console.log(fichierHtml);
+			//afficher le fichier html sur ma page web
+				$('#md').html(fichierHtml);
+			});
 		},
 
 		ajaxMenuFail : function(){
@@ -67,7 +51,6 @@
 
 	$(document).ready(function(){
 		//app.init();
-		
 		app.initMenu();
 	});
 })();
